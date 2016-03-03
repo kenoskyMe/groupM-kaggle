cf58fb5adb96ce20fe0a9b56233d016114088bbd
Kenosky Me <kenosky.me@gmail.com>
2016-03-03 17:08:15 +0000

    data prep intro

---
+++ prepareData.r
@@ -0,0 +1,12 @@
+## This is a script that contains code for Data Preparation
+allState1 <- read.csv('~/R/groupM-kaggle/test_v2.csv', sep=",", header = TRUE, stringsAsFactors = FALSE)
+
+
+## Load the Trainning Dataset
+allState1 <- read.csv('~/R/groupM-kaggle/train.csv', sep=",", header = TRUE, stringsAsFactors = FALSE)
+
+# To see the number of rows
+nrow(allState1)
+
+# taking a sneek pick of the data frame
+head(allState1,2)
